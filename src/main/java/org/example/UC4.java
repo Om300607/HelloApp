c8c372d91a404e285fd6725e0996e0c3d4b8cd55
OM <30raiom@gmail.com>
2026-04-06 08:57:41 +0530

    UC4: Updated greeting to handle multiple names elegantly

--- src/main/java/org/example/UC4.java
+++ src/main/java/org/example/UC4.java
@@ -1,4 +1,11 @@
-package org.example;
-
 public class UC4 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        }
+    }
 }
