794ae5dab70f2a5e19a098e9ef10e979641a420f
OM <30raiom@gmail.com>
2026-04-06 09:25:02 +0530

    UC6: Enhanced for loop with substring to remove trailing delimiter

---
+++ src/main/java/org/example/UC6.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class UC6 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder greeting = new StringBuilder("Hello, ");
+
+            // Enhanced for loop to append names with delimiter
+            for (String name : args) {
+                greeting.append(name).append(", ");
+            }
+
+            // Remove the trailing comma and space using substring
+            String finalGreeting = greeting.substring(0, greeting.length() - 2);
+
+            System.out.println(finalGreeting + "!");
+        }
+    }
+}
