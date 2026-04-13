a3089360dccc01e5b1c38f24a8522eb5b56c8c68
OM <30raiom@gmail.com>
2026-04-13 08:30:17 +0530

    String join

--- src/main/java/org/example/UC7.java
+++ src/main/java/org/example/UC7.java
@@ -1,4 +1,14 @@
 package org.example;
 
 public class UC7 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            // Default greeting when no arguments are passed
+            System.out.println("Hello, World!");
+        } else {
+            // Efficiently join all arguments with a delimiter
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
+        }
+    }
 }
